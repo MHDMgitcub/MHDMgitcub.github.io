fde4b2d283e3baf7880147dde41cde8e6d6bf88b
MHDMgitcub <153813945+MHDMgitcub@users.noreply.github.com>
2023-12-27 19:00:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var backToTopButton = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+});
 
